a5a24dfba3f9bd16236b63c51286f7affb126234
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-11-29 20:08:39 -0800

    features_append: rename method to decrease usage ambiguity

--- src/ReassemblyModifier.jl
+++ src/ReassemblyModifier.jl
@@ -6,9 +6,9 @@ module ReassemblyModifier
 # simple function calls.
 # What does SF need to do?
 # defineBlock( dump in your struct )
-# features_modify_set!
-# features_modify_add!
-# features_modify_remove!
+# features_set!
+# features_append!
+# features_remove!
 # field_modify_multiply!
 # field_modify_add!
 # field_modify_set!
@@ -19,7 +19,7 @@ function features_set!(block, set_features)
     nothing
 end
 
-function features_add!(block, add_features)
+function features_append!(block, add_features)
     if haskey( block, FEATURES_KEY )
         block[ FEATURES_KEY ].append!( add_features )
     else
