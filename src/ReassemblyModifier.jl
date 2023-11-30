427965b3546eb965559f927b76fcb479276bb2ee
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-11-29 23:55:41 -0800

    key ops: add various functions for modifying block fields

--- src/ReassemblyModifier.jl
+++ src/ReassemblyModifier.jl
@@ -54,10 +54,10 @@ function features_remove!(block, remove_features)
     return nothing
 end
 
-function blocks_from_faction(blocks, faction)
+function blocks_from_faction(blocks_full, faction)
     output = []
 
-    for block in blocks
+    for block in blocks_full
         if ( block[ FACTION_KEY ] in faction )
             push!( output, block )
         end
@@ -66,25 +66,62 @@ function blocks_from_faction(blocks, faction)
     return output
 end
 
-function blocks_from_id(blocks, range_id)
+function blocks_from_id(blocks_full, range_id)
     output = []
 
-    for block in blocks
+    for block in blocks_full
         if ( block[ ID_KEY ] in range_id )
             push!( output, block )
         end
     end
 end
 
-function blocks_from_key(blocks, key, range)
+function blocks_from_key(blocks_full, key, range)
     output = []
 
-    for block in blocks
+    for block in blocks_full
         if ( block[ key ] in range )
             push!( output, block )
         end
     end
 end
 
+function key_set!(blocks, keys, value)
+    for block in blocks
+        for key in keys
+            block[ key ] = value
+        end
+    end
+end
+
+function key_bound!(blocks, keys, lower, upper)
+    for block in blocks
+        for key in keys
+            if !haskey( block, key )
+                block[ key ] = lower
+            elseif ( block[ key ] < lower )
+                block[ key ] = lower
+            elseif ( block[ key ] > upper )
+                block[ key ] = upper
+            end
+        end
+    end
+end
+
+function key_multiply!(blocks, keys, multiplier)
+    for block in blocks
+        for key in keys
+            block[ key ] *= multiplier
+        end
+    end
+end
+
+function key_addition!(blocks, keys, addend)
+    for block in blocks
+        for key in keys
+            block[ key ] += addend
+        end
+    end
+end
 
 end
